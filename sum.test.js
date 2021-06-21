e7560879d6d5e37cf6b14cac7cd02999d82b0352
rausouza <raul.souza@capgemini.com>
2021-06-21 13:32:36 -0300

    Testando negação

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@jest/globals');
 const sum = require('./sum');
 
-test('soma 1 + 2 deve ser 3', () => {
+test('sum 1 + 2 deve ser 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
@@ -10,3 +10,11 @@ test('atribuição de objs', () => {
     data['two'] = 2;
     expect(data).toEqual({one: 1, two: 2});
 });
+
+test('testando negação de teste', () => {
+    for(let a = 1; a <= 10; a++) {
+        for(let b = 1; b <= 10; b++) {
+            expect(a+b).not.toBe(0);
+        }
+    }
+})
