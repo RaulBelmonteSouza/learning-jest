afff73d5a140418130febe2cc34c2b9c4460955b
rausouza <raul.souza@capgemini.com>
2021-06-21 13:25:49 -0300

    Testando igualdade de objetos usando toEqual()

--- sum.test.js
+++ sum.test.js
@@ -4,3 +4,9 @@ const sum = require('./sum');
 test('soma 1 + 2 deve ser 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('atribuição de objs', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
